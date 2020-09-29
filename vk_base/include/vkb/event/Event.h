a753c0192319779c10a93ca6ab42307f335de2d7
Nicola Crivellin <nicola.crivellin@avira.com>
2020-09-29 15:49:21 +0200

    Add `on` function to vkb
    A helper to create event listeners.

--- vk_base/include/vkb/event/Event.h
+++ vk_base/include/vkb/event/Event.h
@@ -10,3 +10,13 @@
 #include "WindowEvents.h"
 
 #include "Keys.h"
+#include "InputState.h"
+
+namespace vkb
+{
+    template<typename EventType>
+    inline auto on(std::function<void(const EventType&)> callback) -> UniqueListenerId<EventType>
+    {
+        return EventHandler<EventType>::addListener(std::move(callback));
+    }
+}
