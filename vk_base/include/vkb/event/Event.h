c3298a8ba32dd524240916d90e37084f4b7dd45f
Nicola Crivellin <nicola.crivellin@avira.com>
2020-10-09 18:48:53 +0200

    Return a MaybeUniqueListener wrapper from vkb::on

--- vk_base/include/vkb/event/Event.h
+++ vk_base/include/vkb/event/Event.h
@@ -14,9 +14,51 @@
 
 namespace vkb
 {
+    /**
+     * @brief A wrapper around ListenerIds.
+     *
+     * Conveniently decide whether to create a unique listener handle
+     * or to keep/destroy the non-managing default handle.
+     */
     template<typename EventType>
-    inline auto on(std::function<void(const EventType&)> callback) -> UniqueListenerId<EventType>
+    class MaybeUniqueListener
     {
-        return EventHandler<EventType>::addListener(std::move(callback));
+    public:
+        using IdType = typename EventHandler<EventType>::ListenerId;
+
+        inline MaybeUniqueListener(IdType id) : id(id) {}
+
+        /**
+         * Allow convenient conversion to the default handle type.
+         */
+        inline operator IdType() {
+            return id;
+        }
+
+        /**
+         * Allow convenient conversion to the unique handle type.
+         */
+        inline operator UniqueListenerId<EventType>() {
+            return makeUnique();
+        }
+
+        /**
+         * @brief Create a unique handle from the stored non-unique
+         *        listener handle.
+         *
+         * @return UniqueListenerId<EventType>
+         */
+        inline auto makeUnique() -> UniqueListenerId<EventType> {
+            return { id };
+        }
+
+    private:
+        IdType id;
+    };
+
+    template<typename EventType>
+    inline auto on(std::function<void(const EventType&)> callback) -> MaybeUniqueListener<EventType>
+    {
+        return { EventHandler<EventType>::addListener(std::move(callback)) };
     }
 }
