8c8da3ff2da3ae71654d78a7517f4e418add080d
Nicola Crivellin <nicola.crivellin@avira.com>
2020-10-17 10:45:05 +0200

    Use asset classes as TypesafeID type

--- include/trc/AssetIds.h
+++ include/trc/AssetIds.h
@@ -1,22 +1,17 @@
 #pragma once
 
+#include <vkb/Image.h>
+
 #include "Boilerplate.h"
 #include "utils/TypesafeId.h"
+#include "Geometry.h"
+#include "Material.h"
 
 namespace trc
 {
-    namespace internal
-    {
-        // I don't want to use the asset classes directly for the typesafe IDs.
-        // It feels wrong.
-
-        struct _GeometryAssetIdType {};
-        struct _MaterialAssetIdType {};
-        struct _TextureAssetIdType {};
-    }
-
     using AssetRegistryIdType = ui32;
-    using GeometryID = TypesafeID<internal::_GeometryAssetIdType, AssetRegistryIdType>;
-    using MaterialID = TypesafeID<internal::_MaterialAssetIdType, AssetRegistryIdType>;
-    using TextureID  = TypesafeID<internal::_TextureAssetIdType, AssetRegistryIdType>;
+
+    using GeometryID = TypesafeID<Geometry, AssetRegistryIdType>;
+    using MaterialID = TypesafeID<Material, AssetRegistryIdType>;
+    using TextureID  = TypesafeID<vkb::Image, AssetRegistryIdType>;
 }
