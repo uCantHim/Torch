b178bd7ea1908ac36926b869ad22a0aba95a148b
Nicola Crivellin <nicola.crivellin@avira.com>
2020-11-04 13:37:09 +0100

    Put GlobalRenderDataDescriptor into separate file

---
+++ include/trc/RenderDataDescriptor.h
@@ -0,0 +1,69 @@
+#pragma once
+
+#include <vkb/basics/Swapchain.h>
+#include <vkb/Buffer.h>
+
+#include "DescriptorProvider.h"
+#include "utils/Camera.h"
+
+namespace trc
+{
+    /**
+     * @brief Provides global, renderer-specific data
+     *
+     * Contains the following data:
+     *
+     * - binding 0:
+     *      mat4 currentViewMatrix
+     *      mat4 currentProjMatrix
+     *      mat4 currentInverseViewMatrix
+     *      mat4 currentInverseProjMatrix
+     *
+     * - binding 1:
+     *      vec2 mousePosition          (in pixels)
+     *      vec2 swapchainResolution    (in pixels)
+     */
+    class GlobalRenderDataDescriptor
+    {
+    public:
+        GlobalRenderDataDescriptor();
+
+        auto getProvider() const noexcept -> const DescriptorProviderInterface&;
+
+        void updateCameraMatrices(const Camera& camera);
+        void updateSwapchainData(const vkb::Swapchain& swapchain);
+
+    private:
+        /** Calculates dynamic offsets into the buffer */
+        class RenderDataDescriptorProvider : public DescriptorProviderInterface
+        {
+        public:
+            RenderDataDescriptorProvider(const GlobalRenderDataDescriptor& desc);
+
+            auto getDescriptorSet() const noexcept -> vk::DescriptorSet override;
+            auto getDescriptorSetLayout() const noexcept -> vk::DescriptorSetLayout override;
+            void bindDescriptorSet(
+                vk::CommandBuffer cmdBuf,
+                vk::PipelineBindPoint bindPoint,
+                vk::PipelineLayout pipelineLayout,
+                ui32 setIndex
+            ) const override;
+
+        private:
+            const GlobalRenderDataDescriptor& descriptor;
+        };
+
+        void createResources();
+        void createDescriptors();
+
+        vk::UniqueDescriptorPool descPool;
+        vk::UniqueDescriptorSetLayout descLayout;
+        vk::UniqueDescriptorSet descSet;
+        RenderDataDescriptorProvider provider;
+
+        static constexpr vk::DeviceSize CAMERA_DATA_SIZE{ sizeof(mat4) * 4 };
+        static constexpr vk::DeviceSize SWAPCHAIN_DATA_SIZE{ sizeof(vec2) * 2 };
+        const ui32 BUFFER_SECTION_SIZE; // not static because depends on physical device align
+        vkb::Buffer buffer;
+    };
+} // namespace trc
---
+++ src/RenderDataDescriptor.cpp
@@ -0,0 +1,141 @@
+#include "RenderDataDescriptor.h"
+
+#include "utils/Util.h"
+
+
+
+trc::GlobalRenderDataDescriptor::GlobalRenderDataDescriptor()
+    :
+    provider(*this),
+    BUFFER_SECTION_SIZE(util::pad(
+        CAMERA_DATA_SIZE + SWAPCHAIN_DATA_SIZE,
+        vkb::getPhysicalDevice().properties.limits.minUniformBufferOffsetAlignment
+    ))
+{
+    createResources();
+    createDescriptors();
+}
+
+auto trc::GlobalRenderDataDescriptor::getProvider() const noexcept
+    -> const DescriptorProviderInterface&
+{
+    return provider;
+}
+
+void trc::GlobalRenderDataDescriptor::updateCameraMatrices(const Camera& camera)
+{
+    auto buf = reinterpret_cast<mat4*>(buffer.map(
+        BUFFER_SECTION_SIZE * vkb::getSwapchain().getCurrentFrame(), CAMERA_DATA_SIZE
+    ));
+    buf[0] = camera.getViewMatrix();
+    buf[1] = camera.getProjectionMatrix();
+    buf[2] = inverse(camera.getViewMatrix());
+    buf[3] = inverse(camera.getProjectionMatrix());
+    buffer.unmap();
+}
+
+void trc::GlobalRenderDataDescriptor::updateSwapchainData(const vkb::Swapchain& swapchain)
+{
+    const ui32 currentFrame = vkb::getSwapchain().getCurrentFrame();
+    const vec2 res = vec2(static_cast<float>(swapchain.getImageExtent().width),
+                          static_cast<float>(swapchain.getImageExtent().height));
+
+    auto buf = reinterpret_cast<vec2*>(buffer.map(
+        BUFFER_SECTION_SIZE * currentFrame + CAMERA_DATA_SIZE, SWAPCHAIN_DATA_SIZE
+    ));
+    buf[0] = swapchain.getMousePosition();
+    buf[1] = res;
+    buffer.unmap();
+}
+
+void trc::GlobalRenderDataDescriptor::createResources()
+{
+    buffer = vkb::Buffer(
+        BUFFER_SECTION_SIZE * vkb::getSwapchain().getFrameCount(),
+        vk::BufferUsageFlagBits::eUniformBuffer,
+        vk::MemoryPropertyFlagBits::eHostVisible | vk::MemoryPropertyFlagBits::eHostCoherent
+    );
+}
+
+void trc::GlobalRenderDataDescriptor::createDescriptors()
+{
+    // Create descriptors
+    descPool = vkb::getDevice()->createDescriptorPoolUnique(
+        vk::DescriptorPoolCreateInfo(
+            vk::DescriptorPoolCreateFlagBits::eFreeDescriptorSet,
+            vkb::getSwapchain().getFrameCount(),
+            std::vector<vk::DescriptorPoolSize>{
+                { vk::DescriptorType::eUniformBufferDynamic, 2 }
+            }
+    ));
+
+    descLayout = vkb::getDevice()->createDescriptorSetLayoutUnique(
+        vk::DescriptorSetLayoutCreateInfo({},
+            std::vector<vk::DescriptorSetLayoutBinding>{
+                {
+                    0, vk::DescriptorType::eUniformBufferDynamic, 1,
+                    vk::ShaderStageFlagBits::eAllGraphics
+                },
+                {
+                    1, vk::DescriptorType::eUniformBufferDynamic, 1,
+                    vk::ShaderStageFlagBits::eAllGraphics
+                },
+            }
+    ));
+
+    descSet = std::move(vkb::getDevice()->allocateDescriptorSetsUnique(
+        vk::DescriptorSetAllocateInfo(*descPool, *descLayout)
+    )[0]);
+
+    // Update descriptor set
+    vk::DescriptorBufferInfo cameraInfo(*buffer, 0, CAMERA_DATA_SIZE);
+    vk::DescriptorBufferInfo swapchainInfo(*buffer, CAMERA_DATA_SIZE, SWAPCHAIN_DATA_SIZE);
+    std::vector<vk::WriteDescriptorSet> writes{
+        vk::WriteDescriptorSet(
+            *descSet, 0, 0, 1,
+            vk::DescriptorType::eUniformBufferDynamic,
+            nullptr, &cameraInfo, nullptr
+        ),
+        vk::WriteDescriptorSet(
+            *descSet, 1, 0, 1,
+            vk::DescriptorType::eUniformBufferDynamic,
+            nullptr, &swapchainInfo, nullptr
+        ),
+    };
+    vkb::getDevice()->updateDescriptorSets(writes, {});
+}
+
+trc::GlobalRenderDataDescriptor::RenderDataDescriptorProvider::RenderDataDescriptorProvider(
+    const GlobalRenderDataDescriptor& desc)
+    :
+    descriptor(desc)
+{
+}
+
+auto trc::GlobalRenderDataDescriptor::RenderDataDescriptorProvider::getDescriptorSet()
+    const noexcept -> vk::DescriptorSet
+{
+    return *descriptor.descSet;
+}
+
+auto trc::GlobalRenderDataDescriptor::RenderDataDescriptorProvider::getDescriptorSetLayout()
+    const noexcept -> vk::DescriptorSetLayout
+{
+    return *descriptor.descLayout;
+}
+
+void trc::GlobalRenderDataDescriptor::RenderDataDescriptorProvider::bindDescriptorSet(
+    vk::CommandBuffer cmdBuf,
+    vk::PipelineBindPoint bindPoint,
+    vk::PipelineLayout pipelineLayout,
+    ui32 setIndex) const
+{
+    const ui32 dynamicOffset = descriptor.BUFFER_SECTION_SIZE
+                               * vkb::getSwapchain().getCurrentFrame();
+
+    cmdBuf.bindDescriptorSets(
+        bindPoint, pipelineLayout, setIndex,
+        *descriptor.descSet,
+        { dynamicOffset, dynamicOffset }
+    );
+}
