1607118d0ceb05f8f8c950e366370f6890efa991
Nicola Crivellin <nicola.crivellin@avira.com>
2021-01-09 17:20:15 +0100

    Add ExclusiveQueue to vk_base

---
+++ vk_base/include/vkb/ExclusiveQueue.h
@@ -0,0 +1,84 @@
+#pragma once
+
+#include <queue>
+#include <functional>
+#include <thread>
+#include <atomic>
+#include <mutex>
+
+#include <vulkan/vulkan.hpp>
+
+namespace vkb
+{
+    class ExclusiveQueue
+    {
+    public:
+        explicit ExclusiveQueue(vk::Queue queue);
+        ~ExclusiveQueue() = default;
+
+        ExclusiveQueue(const ExclusiveQueue&) = delete;
+        ExclusiveQueue(ExclusiveQueue&&) noexcept = delete;
+        auto operator=(const ExclusiveQueue&) -> ExclusiveQueue& = delete;
+        auto operator=(ExclusiveQueue&&) noexcept -> ExclusiveQueue& = delete;
+
+        inline auto operator*() const noexcept -> vk::Queue {
+            return queue;
+        }
+
+        /**
+         * @brief Calls ExclusiveQueue::submit
+         *
+         * @throw std::runtime_error if current thread doesn't own queue
+         */
+        auto operator<<(const vk::SubmitInfo& submit) -> ExclusiveQueue&;
+
+        /**
+         * @throw std::runtime_error if current thread doesn't own queue
+         */
+        void submit(const vk::ArrayProxy<const vk::SubmitInfo>& submits, vk::Fence fence) const;
+
+        /**
+         * Returns false and does nothing if queue is being owned by another
+         * thread. Submits the work and return true otherwise.
+         */
+        bool trySubmit(const vk::ArrayProxy<const vk::SubmitInfo>& submits, vk::Fence fence) const;
+
+        /**
+         * Waits until queue is free, then transfers ownership to current
+         * thread, then submits work. Blocks submission of new work while
+         * waiting.
+         *
+         * Has the same overhead as `submit` or `trySubmit` when the calling
+         * thread already owns the queue.
+         *
+         * This can never fail but it may wait a while until the queue is
+         * free.
+         */
+        void waitSubmit(const vk::ArrayProxy<const vk::SubmitInfo>& submits, vk::Fence fence);
+
+        /**
+         * @brief Transfers queue ownership to current thread
+         *
+         * Waits until the queue is idle, then changes ownership
+         */
+        void transferOwnership(std::thread::id newOwner);
+
+        /**
+         * @param std::thread::id thread
+         *
+         * @return bool True if the specified thread currently owns the
+         *              queue
+         */
+        bool hasOwnership(std::thread::id thread) const noexcept;
+
+        void waitIdle() const;
+
+    private:
+        void doSubmit(const vk::ArrayProxy<const vk::SubmitInfo>& submits, vk::Fence fence) const;
+
+        vk::Queue queue;
+
+        mutable std::mutex submissionLock;
+        std::thread::id currentThread{ std::this_thread::get_id() };
+    };
+} // namespace vkb
---
+++ vk_base/src/ExclusiveQueue.cpp
@@ -0,0 +1,82 @@
+#include "ExclusiveQueue.h"
+
+#include <string>
+#include <sstream>
+
+
+
+vkb::ExclusiveQueue::ExclusiveQueue(vk::Queue queue)
+    :
+    queue(queue)
+{
+}
+
+auto vkb::ExclusiveQueue::operator<<(const vk::SubmitInfo& submit) -> ExclusiveQueue&
+{
+    this->submit(submit, {});
+    return *this;
+}
+
+void vkb::ExclusiveQueue::submit(
+    const vk::ArrayProxy<const vk::SubmitInfo>& submits,
+    vk::Fence fence) const
+{
+    std::lock_guard lock(submissionLock);
+    if (std::this_thread::get_id() != currentThread)
+    {
+        std::stringstream ss;
+        ss << "Tried to submit work in thread " << std::this_thread::get_id()
+            << ", but queue is currently owned by thread " << currentThread;
+        throw std::runtime_error(ss.str());
+    }
+
+    doSubmit(submits, fence);
+}
+
+bool vkb::ExclusiveQueue::trySubmit(
+    const vk::ArrayProxy<const vk::SubmitInfo>& submits,
+    vk::Fence fence) const
+{
+    std::lock_guard lock(submissionLock);
+    if (std::this_thread::get_id() != currentThread) {
+        return false;
+    }
+
+    doSubmit(submits, fence);
+    return true;
+}
+
+void vkb::ExclusiveQueue::waitSubmit(
+    const vk::ArrayProxy<const vk::SubmitInfo>& submits,
+    vk::Fence fence)
+{
+    std::lock_guard lock(submissionLock);
+    transferOwnership(std::this_thread::get_id());
+    doSubmit(submits, fence);
+}
+
+void vkb::ExclusiveQueue::transferOwnership(std::thread::id newOwner)
+{
+    if (newOwner != currentThread)
+    {
+        currentThread = newOwner;
+        queue.waitIdle();
+    }
+}
+
+bool vkb::ExclusiveQueue::hasOwnership(std::thread::id thread) const noexcept
+{
+    return currentThread == thread;
+}
+
+void vkb::ExclusiveQueue::doSubmit(
+    const vk::ArrayProxy<const vk::SubmitInfo>& submits,
+    vk::Fence fence) const
+{
+    queue.submit(submits, fence);
+}
+
+void vkb::ExclusiveQueue::waitIdle() const
+{
+    queue.waitIdle();
+}
