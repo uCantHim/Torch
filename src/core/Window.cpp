f0386d6ef2ba1eec9524eeecdbe95434bd6015e0
Nicola Crivellin <nicola.crivellin@avira.com>
2021-08-31 17:17:52 +0200

    Always specify storage image bit for swapchain

--- src/core/Window.cpp
+++ src/core/Window.cpp
@@ -8,11 +8,12 @@ trc::Window::Window(Instance& instance, WindowCreateInfo info)
     :
     instance(&instance),
     swapchain([&] {
+        // Always specify the storage bit
+        info.swapchainCreateInfo.imageUsage |= vk::ImageUsageFlagBits::eStorage;
+
         // Additional flags currently only used for ray tracing
-        if (instance.hasRayTracing())
-        {
-            info.swapchainCreateInfo.imageUsage |= vk::ImageUsageFlagBits::eTransferDst
-                                                   | vk::ImageUsageFlagBits::eStorage;
+        if (instance.hasRayTracing()) {
+            info.swapchainCreateInfo.imageUsage |= vk::ImageUsageFlagBits::eTransferDst;
         }
 
         return vkb::Swapchain(
