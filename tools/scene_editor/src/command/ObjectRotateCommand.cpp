2ce0d86ab4e28056cd7e9ea11652513c98bfba99
Nicola Crivellin <nicola.crivellin@avira.com>
2022-02-20 16:12:52 +0100

    Implement ObjectRotateCommand

--- tools/scene_editor/src/command/ObjectRotateCommand.cpp
+++ tools/scene_editor/src/command/ObjectRotateCommand.cpp
@@ -6,43 +6,89 @@
 #include "AxisFlags.h"
 
 
+
 class ObjectRotateState : public CommandState
 {
 public:
     ObjectRotateState(SceneObject obj, Scene& scene)
         :
         obj(obj),
-        scene(&scene)
+        scene(&scene),
+        originalOrientation(scene.get<ObjectBaseNode>(obj).getRotation()),
+        pivot(scene.get<ObjectBaseNode>(obj).getGlobalTransform()[3]),
+        depth(scene.getCamera().calcScreenDepth(pivot)),
+        originalDir(glm::normalize(scene.getMousePosAtDepth(depth) - pivot)),
+        rotationAxis(glm::normalize(pivot - vec3(scene.getCamera().getViewMatrix()[3])))
     {
+        assert(glm::all(glm::not_(glm::isnan(originalDir))));
+        assert(glm::all(glm::not_(glm::isnan(rotationAxis))));
+        assert(glm::all(glm::not_(glm::isnan(pivot))));
     }
 
     bool update(const float) override
     {
-        return true;
+        const quat rotation = glm::angleAxis(getNewAngle(), rotationAxis) * originalOrientation;
+        scene->get<ObjectBaseNode>(obj).setRotation(rotation);
+
+        return terminate;
     }
 
     void onExit() override
     {
+        const quat rotation = glm::angleAxis(finalAngle, rotationAxis) * originalOrientation;
+        scene->get<ObjectBaseNode>(obj).setRotation(rotation);
     }
 
-    void applyScaling()
+    void applyRotation()
     {
+        finalAngle = getNewAngle();
+        terminate = true;
     }
 
-    void resetScaling()
+    void resetRotation()
     {
+        finalAngle = 0.0f;
+        terminate = true;
     }
 
     void lockAxes(AxisFlags flags)
     {
-        rotationAxis = toVector(flags);
+        assert(glm::length(toVector(flags)) > 0.0f);
+        rotationAxis = glm::normalize(toVector(flags));
     }
 
 private:
+    auto getNewAngle() const -> float
+    {
+        vec3 dir = scene->getMousePosAtDepth(depth) - pivot;
+        if (float len = glm::length(dir); len > 0.0f) {
+            dir /= len;
+        }
+        else {
+            return 0.0f;
+        }
+
+        const vec3 cross = glm::cross(dir, originalDir);
+        const float diff = glm::dot(dir, originalDir);
+        if (glm::abs(diff) == 1.0f) {
+            return 0.0f;
+        }
+
+        return -glm::sign(cross.x + cross.y + cross.z) * glm::acos(diff);
+    }
+
     const SceneObject obj;
     Scene* scene;
 
-    vec3 rotationAxis{ 1, 1, 1 };
+    const quat originalOrientation;
+
+    const vec3 pivot;
+    const float depth;
+    const vec3 originalDir;
+    vec3 rotationAxis;
+
+    bool terminate{ false };
+    float finalAngle{ 0.0f };
 };
 
 
@@ -54,17 +100,18 @@ void ObjectRotateCommand::execute(CommandCall& call)
     {
         auto& state = call.setState(ObjectRotateState{ obj, scene });
 
-        call.on(vkb::Key::escape,        [&](auto&){ state.resetScaling(); });
-        call.on(vkb::MouseButton::right, [&](auto&){ state.resetScaling(); });
-        call.on(vkb::Key::enter,         [&](auto&){ state.applyScaling(); });
-        call.on(vkb::MouseButton::left,  [&](auto&){ state.applyScaling(); });
+        call.on(vkb::Key::escape,        [&](auto&){ state.resetRotation(); });
+        call.on(vkb::MouseButton::right, [&](auto&){ state.resetRotation(); });
+        call.on(vkb::Key::enter,         [&](auto&){ state.applyRotation(); });
+        call.on(vkb::MouseButton::left,  [&](auto&){ state.applyRotation(); });
 
         // x and y keys are swapped because it seems like glfw uses the american keyboard (why?)
+        auto shift = vkb::KeyModFlagBits::shift;
         call.on({ vkb::Key::x }, [&](auto&){ state.lockAxes(Axis::eY | Axis::eZ); });
         call.on({ vkb::Key::z }, [&](auto&){ state.lockAxes(Axis::eX | Axis::eZ); });
         call.on({ vkb::Key::y }, [&](auto&){ state.lockAxes(Axis::eX | Axis::eY); });
-        call.on({ vkb::Key::x, vkb::KeyModFlagBits::shift }, [&](auto&){ state.lockAxes(Axis::eX); });
-        call.on({ vkb::Key::z, vkb::KeyModFlagBits::shift }, [&](auto&){ state.lockAxes(Axis::eY); });
-        call.on({ vkb::Key::y, vkb::KeyModFlagBits::shift }, [&](auto&){ state.lockAxes(Axis::eZ); });
+        call.on({ vkb::Key::x, shift }, [&](auto&){ state.lockAxes(Axis::eY | Axis::eZ); });
+        call.on({ vkb::Key::z, shift }, [&](auto&){ state.lockAxes(Axis::eX | Axis::eZ); });
+        call.on({ vkb::Key::y, shift }, [&](auto&){ state.lockAxes(Axis::eX | Axis::eY); });
     };
 }
