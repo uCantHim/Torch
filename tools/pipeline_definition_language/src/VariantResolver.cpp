db4293631d99fadba3be5d1a417553a15db8e688
Nicola Crivellin <nicola.crivellin98@gmail.com>
2022-06-12 00:38:31 +0200

    [se] Fix variant resolver for nested match expressions
    The correct solution is much more elaborate than what I wrote initially.
    It now handles nested matches, partly nested matches, and nested
    duplicate flags.

--- tools/pipeline_definition_language/src/VariantResolver.cpp
+++ tools/pipeline_definition_language/src/VariantResolver.cpp
@@ -9,6 +9,24 @@
 
 
 
+VariantResolver::ValueVariantSet::ValueVariantSet(std::vector<FieldValueVariant> _vars)
+    :
+    variants(std::move(_vars))
+{
+    for (const auto& var : this->variants) {
+        for (const auto& flag : var.setFlags) {
+            flagTypes.emplace(flag.flagId);
+        }
+    }
+}
+
+bool VariantResolver::ValueVariantSet::hasFlagType(VariantFlag flag) const
+{
+    return flagTypes.contains(flag.flagId);
+}
+
+
+
 VariantResolver::VariantResolver(const FlagTable& flags, const IdentifierTable& ids)
     :
     flagTable(flags),
@@ -18,17 +36,17 @@ VariantResolver::VariantResolver(const FlagTable& flags, const IdentifierTable& 
 
 auto VariantResolver::resolve(FieldValue& value) -> std::vector<FieldValueVariant>
 {
-    return std::visit(*this, value);
+    return std::visit(*this, value).variants;
 }
 
-auto VariantResolver::operator()(const LiteralValue& val) const -> std::vector<FieldValueVariant>
+auto VariantResolver::operator()(const LiteralValue& val) const -> ValueVariantSet
 {
     std::vector<FieldValueVariant> res;
     res.push_back({ .setFlags={}, .value=val });
     return res;
 }
 
-auto VariantResolver::operator()(const Identifier& id) const -> std::vector<FieldValueVariant>
+auto VariantResolver::operator()(const Identifier& id) const -> ValueVariantSet
 {
     if (!identifierTable.has(id)) {
         throw InternalLogicError("Identifier \"" + id.name + "\" is not present in identifier table.");
@@ -36,7 +54,7 @@ auto VariantResolver::operator()(const Identifier& id) const -> std::vector<Fiel
 
     const IdentifierValue& referenced = identifierTable.get(id);
     return std::visit(VariantVisitor{
-        [this, &id](const ValueReference& ref)
+        [this, &id](const ValueReference& ref) -> ValueVariantSet
         {
             assert(ref.referencedValue != nullptr);
             auto variants = std::visit(*this, *ref.referencedValue);
@@ -45,17 +63,17 @@ auto VariantResolver::operator()(const Identifier& id) const -> std::vector<Fiel
             }
             return variants;
         },
-        [](const TypeName&) -> std::vector<FieldValueVariant> {
+        [](const TypeName&) -> ValueVariantSet {
             throw InternalLogicError("Tried to resolve variants on an identifier value that"
                                      " was a type name.");
         },
-        [&id](const DataConstructor&) -> std::vector<FieldValueVariant> {
-            return { FieldValueVariant{ .setFlags={}, .value=id } };
+        [&id](const DataConstructor&) -> ValueVariantSet {
+            return { { FieldValueVariant{ .setFlags={}, .value=id } } };
         },
     }, referenced);
 }
 
-auto VariantResolver::operator()(const ListDeclaration& list) const -> std::vector<FieldValueVariant>
+auto VariantResolver::operator()(const ListDeclaration& list) const -> ValueVariantSet
 {
     std::vector<FieldValueVariant> results;
     results.push_back({ {}, list });
@@ -91,7 +109,7 @@ auto VariantResolver::operator()(const ListDeclaration& list) const -> std::vect
     return results;
 }
 
-auto VariantResolver::operator()(const ObjectDeclaration& obj) const -> std::vector<FieldValueVariant>
+auto VariantResolver::operator()(const ObjectDeclaration& obj) const -> ValueVariantSet
 {
     std::vector<FieldValueVariant> results;
     results.push_back({ {}, obj });
@@ -128,33 +146,131 @@ auto VariantResolver::operator()(const ObjectDeclaration& obj) const -> std::vec
     return results;
 }
 
-auto VariantResolver::operator()(const MatchExpression& expr) const -> std::vector<FieldValueVariant>
+auto VariantResolver::operator()(const MatchExpression& expr) const -> ValueVariantSet
 {
+    /**
+     * Test if a flag set contains a specific flag bit.
+     */
+    auto hasSameBit = [](const VariantFlagSet& set, const VariantFlag& flag) -> bool
+    {
+        return std::find_if(
+            set.begin(), set.end(),
+            [&flag](auto&& other){
+                return other.flagId == flag.flagId && other.flagBitId == flag.flagBitId;
+            }
+        ) != set.end();
+    };
+
     const auto flagName = expr.matchedType.name;
 
-    std::vector<FieldValueVariant> results;
+    // First pass to collect all required flag types
+    std::vector<ValueVariantSet> caseVariants;  // Collect all variants once
+    std::unordered_set<size_t> newFlagTypes;
     for (const auto& opt : expr.cases)
     {
         const auto currentOptFlag = flagTable.getRef(flagName, opt.caseIdentifier.name);
-        auto values = std::visit(*this, *opt.value);
-        for (auto& value : values)
-        {
-            // Add current flag if not already by match lower in the tree
-            if (value.setFlags.end() == std::ranges::find(value.setFlags, currentOptFlag)) {
-                value.setFlags.emplace_back(currentOptFlag);
-            }
+        auto& vars = caseVariants.emplace_back(std::visit(*this, *opt.value));
+        newFlagTypes.insert(vars.flagTypes.begin(), vars.flagTypes.end());
+        newFlagTypes.erase(currentOptFlag.flagId);  // Don't contain the current flag
+    }
 
-            results.emplace_back(std::move(value));
+    // Second pass to generate variants
+    std::vector<FieldValueVariant> results;
+    for (size_t i = 0; const auto& opt : expr.cases)
+    {
+        const auto currentOptFlag = flagTable.getRef(flagName, opt.caseIdentifier.name);
+        auto& baseVariants = caseVariants.at(i++);
+
+        if (baseVariants.flagTypes.contains(currentOptFlag.flagId))
+        {
+            // The case's subtree has already been matched on the current match expression's
+            // flag. This case only allows the variants of the same flag type that have the same
+            // *flag bit*.
+            for (auto& var : baseVariants)
+            {
+                if (hasSameBit(var.setFlags, currentOptFlag))
+                {
+                    std::ranges::move(
+                        generateVariants(newFlagTypes, std::move(var)),
+                        std::back_inserter(results)
+                    );
+                }
+            }
+        }
+        else {
+            // The flag type of this match expression is new for the case's subtree.
+            for (auto& var : baseVariants)
+            {
+                var.setFlags.emplace_back(currentOptFlag);
+                std::ranges::move(generateVariants(newFlagTypes, std::move(var)),
+                                  std::back_inserter(results));
+            }
         }
     }
+
     return results;
+}
+
+auto VariantResolver::generateVariants(
+    const std::unordered_set<size_t>& requiredFlagTypes,
+    FieldValueVariant&& _variant) const
+    -> std::vector<FieldValueVariant>
+{
+    auto hasFlagType = [](const VariantFlagSet& set, const VariantFlag& flag) -> bool
+    {
+        return std::find_if(
+            set.begin(), set.end(),
+            [&flag](auto&& other){ return other.flagId == flag.flagId; }
+        ) != set.end();
+    };
+
+    std::vector<FieldValueVariant> variants{ std::move(_variant) };
+    for (const size_t flagType : requiredFlagTypes)
+    {
+#ifndef NDEBUG  // Debug asserts
+        for (auto& var : variants) {
+            assert(var.setFlags == variants.front().setFlags
+                   && "All variants of a field value must have the same flags set");
+        }
+#endif
+
+        const size_t numBits = flagTable.getNumFlagBits({ flagType, 0 });
+
+        std::vector<FieldValueVariant> newVariants;
+        for (auto& var : variants)
+        {
+            // Don't add if subtree has already been matched on the flag
+            if (hasFlagType(var.setFlags, { flagType, 0 })) continue;
+
+            // The flag type is new for the subtree - variate it over all bits of the
+            // new flag type
+            for (size_t flagBit = 0; flagBit < numBits; ++flagBit)
+            {
+                const VariantFlag currentFlag{ flagType, flagBit };
+
+                auto copy = var;
+                copy.setFlags.emplace_back(currentFlag);
+                newVariants.emplace_back(std::move(copy));
+            }
+        }
+        if (!newVariants.empty()) {
+            std::swap(variants, newVariants);
+        }
+    }
+
+    return variants;
 }
 
 bool VariantResolver::isVariantOfSameFlag(const FieldValueVariant& a, const FieldValueVariant& b)
 {
-    for (auto& fa : a.setFlags)
+    return isVariantOfSameFlag(a.setFlags, b.setFlags);
+}
+
+bool VariantResolver::isVariantOfSameFlag(const VariantFlagSet& a, const VariantFlagSet& b)
+{
+    for (auto& fa : a)
     {
-        for (auto& fb : b.setFlags)
+        for (auto& fb : b)
         {
             if (fa.flagId == fb.flagId) {
                 return true;
--- tools/pipeline_definition_language/src/VariantResolver.h
+++ tools/pipeline_definition_language/src/VariantResolver.h
@@ -27,21 +27,44 @@ struct FieldValueVariant
 class VariantResolver
 {
 public:
+    class ValueVariantSet
+    {
+    public:
+        ValueVariantSet(std::vector<FieldValueVariant> vars);
+
+        auto begin() { return variants.begin(); }
+        auto end() { return variants.end(); }
+
+        bool hasFlagType(VariantFlag flag) const;
+
+        std::unordered_set<size_t> flagTypes;
+        std::vector<FieldValueVariant> variants;
+    };
+
     VariantResolver(const FlagTable& flags, const IdentifierTable& ids);
 
     auto resolve(FieldValue& value) -> std::vector<FieldValueVariant>;
 
-    auto operator()(const LiteralValue& val) const -> std::vector<FieldValueVariant>;
-    auto operator()(const Identifier& id) const -> std::vector<FieldValueVariant>;
-    auto operator()(const ListDeclaration& list) const -> std::vector<FieldValueVariant>;
-    auto operator()(const ObjectDeclaration& obj) const -> std::vector<FieldValueVariant>;
-    auto operator()(const MatchExpression& expr) const -> std::vector<FieldValueVariant>;
+    auto operator()(const LiteralValue& val) const -> ValueVariantSet;
+    auto operator()(const Identifier& id) const -> ValueVariantSet;
+    auto operator()(const ListDeclaration& list) const -> ValueVariantSet;
+    auto operator()(const ObjectDeclaration& obj) const -> ValueVariantSet;
+    auto operator()(const MatchExpression& expr) const -> ValueVariantSet;
 
 private:
     static bool isVariantOfSameFlag(const FieldValueVariant& a,
                                     const FieldValueVariant& b);
+    static bool isVariantOfSameFlag(const VariantFlagSet& a,
+                                    const VariantFlagSet& b);
     static void mergeFlags(VariantFlagSet& dst,
                            const VariantFlagSet& src);
+
+    /**
+     * Helper that inflates a variant value to a list of additional flag types.
+     */
+    auto generateVariants(const std::unordered_set<size_t>& requiredFlagTypes,
+                          FieldValueVariant&& variant) const
+        -> std::vector<FieldValueVariant>;
 
     const FlagTable& flagTable;
     const IdentifierTable& identifierTable;
