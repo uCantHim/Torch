7195afe230925005f65ba4a8fdd73fbb6e7ff514
Nicola Crivellin <nicola.crivellin98@gmail.com>
2022-07-12 20:48:08 +0200

    Add CMake function `link_gtest` that discovers gtest

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -12,3 +12,26 @@ function (torch_default_compile_options TARGET)
         target_compile_options(${TARGET} PRIVATE /W4)
     endif()
 endfunction()
+
+function (link_gtest TARGET)
+    find_package(GTest)
+    if (${GTEST_FOUND})
+        target_link_libraries(${TARGET} PRIVATE GTest::GTest GTest::Main)
+    else ()
+        if (NOT TARGET gtest_main)
+            FetchContent_Declare(
+                googletest
+                GIT_REPOSITORY https://github.com/google/googletest
+                GIT_TAG main
+            )
+            # For Windows: Prevent overriding the parent project's compiler/linker settings
+            set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
+
+            set(BUILD_GMOCK FALSE)
+            set(INSTALL_GTEST FALSE)
+            FetchContent_MakeAvailable(googletest)
+        endif ()
+
+        target_link_libraries(${TARGET} PRIVATE gtest_main)
+    endif ()
+endfunction()
