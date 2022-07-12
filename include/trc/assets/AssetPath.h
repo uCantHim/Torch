4d81ba2febde301bcaf5adda5e9a8dbc7116891a
Nicola Crivellin <nicola.crivellin98@gmail.com>
2022-07-12 17:38:49 +0200

    Improve AssetPath implementation
    Add comments. Declare special member functions. Remove the cryptic
    InvalidAssetPathError exception class in favor of std::invalid_argument.

--- include/trc/assets/AssetPath.h
+++ include/trc/assets/AssetPath.h
@@ -10,22 +10,54 @@ namespace trc
 {
     namespace fs = std::filesystem;
 
-    class InvalidAssetPathError : public Exception
-    {
-    public:
-        InvalidAssetPathError(fs::path pathlet, std::string error);
-    };
-
+    /**
+     * @brief A path to an asset file
+     *
+     * A logical path to an internal asset file, relative to the asset
+     * storage directory. Can be converted to a filesystem path.
+     */
     class AssetPath
     {
     public:
-        /**
-         * @throw InvalidAssetPathError if no valid AssetPath can be constructed.
-         */
-        AssetPath(fs::path path);
+        AssetPath(const AssetPath&) = default;
+        AssetPath(AssetPath&&) noexcept = default;
+        AssetPath& operator=(const AssetPath&) = default;
+        AssetPath& operator=(AssetPath&&) noexcept = default;
+        ~AssetPath() = default;
 
+        /**
+         * @param fs::path path Can be one of the following:
+         *  - A relative path: The path will be interpreted as relative
+         *    to the asset directory.
+         *  - An absolute path: Absolute paths MUST contain the asset
+         *    directory as a prefix.
+         *
+         * @throw std::invalid_argument if no valid AssetPath can be
+         *        constructed.
+         */
+        explicit AssetPath(fs::path path);
+
+        /**
+         * @return std::string A string that identifies an asset uniquely
+         *                     based on its storage path.
+         */
         auto getUniquePath() const -> std::string;
+
+        /**
+         * @return fs::path An absolute path to the asset file
+         */
         auto getFilesystemPath() const -> fs::path;
+
+        /**
+         * @brief Retrieve an asset's name.
+         *
+         * The name is a string that identifies an asset, though it is not
+         * necessarily unique! This is mostly useful for user interaction.
+         *
+         * Effectively, this is the filename without its extension.
+         *
+         * @return std::string The asset's name.
+         */
         auto getAssetName() const -> std::string;
 
         auto operator<=>(const AssetPath&) const = default;
--- src/assets/AssetPath.cpp
+++ src/assets/AssetPath.cpp
@@ -4,33 +4,28 @@
 
 
 
-trc::InvalidAssetPathError::InvalidAssetPathError(fs::path pathlet, std::string error)
-    : Exception("Unable to construct unique asset path from \""
-                + pathlet.string() + "\": " + error)
-{
-}
-
-
-
 trc::AssetPath::AssetPath(fs::path path)
 {
     if (path.empty()) {
-        throw InvalidAssetPathError(path, "Path is empty");
+        throw std::invalid_argument("Unable to construct unique asset path from \""
+                                    + path.string() + "\": " + "Path is empty.");
     }
 
     path = path.lexically_normal();
 
     // Replace any root directory names
     if (path.has_root_path()) {
-        path = path.string().replace(0, path.root_path().string().size(), "");
+        path = path.string().substr(path.root_path().string().size());
     }
 
     // Ensure that path is actually a subdirectory of the asset root
     const auto isSubdir = !fs::path(".").lexically_relative(path).empty();
     if (!isSubdir)
     {
-        throw InvalidAssetPathError(path, "Path is outside of asset root directory "
-                                          + util::getAssetStorageDirectory().string());
+        throw std::invalid_argument(
+            "Unable to construct unique asset path from \"" + path.string() + "\": "
+            + "Path is outside of asset root directory "
+            + util::getAssetStorageDirectory().string());
     }
 
     pathlet = std::move(path);
