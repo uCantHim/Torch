cb60f99c1f5f0438989afbf9391f03378e2e798b
Nicola Crivellin <nicola.crivellin98@gmail.com>
2022-10-10 14:44:57 +0200

    Throw in index optimizer if mesh is not supported
    This can happen if a vertex is shared by at least UINT8_MAX triangles.

--- include/trc/util/TriangleCacheOptimizer.h
+++ include/trc/util/TriangleCacheOptimizer.h
@@ -6,6 +6,9 @@
 
 namespace trc::util
 {
+    /**
+     * @throw std::invalid_argument if the mesh type is not supported
+     */
     auto optimizeTriangleOrderingForsyth(const std::vector<ui32>& indices)
         -> std::vector<ui32>;
 } // namespace trc::util
--- src/util/TriangleCacheOptimizer.cpp
+++ src/util/TriangleCacheOptimizer.cpp
@@ -12,6 +12,12 @@ auto optimizeTriangleOrderingForsyth(const std::vector<ui32>& indices) -> std::v
 {
     assert(indices.size() % 3 == 0);
     ui32* result = reorderForsyth(indices.data(), indices.size() / 3, indices.size());
+    if (result == nullptr)
+    {
+        throw std::invalid_argument("[In optimizeTriangleOrderingForsyth]: Unsupported mesh. At"
+                                    " least one vertex is shared by more than "
+                                    + std::to_string(MAX_ADJACENCY) + " triangles.");
+    }
 
     std::vector<ui32> vec(indices.size());
     memcpy(vec.data(), result, sizeof(ui32) * indices.size());
