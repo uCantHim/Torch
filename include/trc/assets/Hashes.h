ec5c06cc97c24553eaa69f5850eb2e4111a35870
Nicola Crivellin <nicola.crivellin98@gmail.com>
2022-10-21 16:55:59 +0200

    Add hashes for TypedAssetId<> and AssetReference<>
    Specialize std::hash<> and implement operator== for the types mentioned
    above. Put the stuff in trc/assets/Hashes.h.

---
+++ include/trc/assets/Hashes.h
@@ -0,0 +1,58 @@
+#pragma once
+
+#include <functional>
+
+#include "trc/assets/AssetID.h"
+#include "trc/assets/AssetReference.h"
+
+template<trc::AssetBaseType T>
+struct std::hash<trc::TypedAssetID<T>>
+{
+    auto operator()(const trc::TypedAssetID<T>& id) const -> size_t
+    {
+        return std::hash<trc::ui32>{}(static_cast<trc::ui32>(id.getDeviceID()));
+    }
+};
+
+template<trc::AssetBaseType T>
+struct std::hash<trc::AssetReference<T>>
+{
+    auto operator()(const trc::AssetReference<T>& ref) const -> size_t
+    {
+        if (ref.empty()) return 0;
+        if (ref.hasResolvedID()) {
+            return std::hash<trc::TypedAssetID<T>>{}(ref.getID());
+        }
+
+        assert(ref.hasAssetPath());
+        return std::hash<trc::AssetPath>{}(ref.getAssetPath());
+    }
+};
+
+namespace trc
+{
+    template<AssetBaseType T>
+    inline bool operator==(const TypedAssetID<T>& a, const TypedAssetID<T>& b)
+    {
+        if (a.getAssetID() == b.getAssetID()) {
+            assert(a.getDeviceID() == b.getDeviceID());
+        }
+
+        return a.getAssetID() == b.getAssetID();
+    }
+
+    template<AssetBaseType T>
+    inline bool operator==(const AssetReference<T>& a,
+                           const AssetReference<T>& b)
+    {
+        if (a.empty() && b.empty()) {
+            return true;
+        }
+        if (a.hasResolvedID() && b.hasResolvedID()) {
+            return a.getID() == b.getID();
+        }
+
+        assert(a.hasAssetPath() && b.hasAssetPath());
+        return a.getAssetPath() == b.getAssetPath();
+    }
+}
