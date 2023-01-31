309166134b064bb13824cb98ebb2946372b55549
Nicola Crivellin <nicola.crivellin98@gmail.com>
2023-01-31 22:27:42 +0100

    Add GuardedSharedRange
    An iterator range that is guarded by a shared lock (as opposed to
    GuardedRange, which is guarded by a unique lock).

--- tools/torch_util/include/trc_util/algorithm/IteratorRange.h
+++ tools/torch_util/include/trc_util/algorithm/IteratorRange.h
@@ -2,6 +2,7 @@
 
 #include <memory>
 #include <mutex>
+#include <shared_mutex>
 
 namespace trc::algorithm
 {
@@ -28,25 +29,57 @@ namespace trc::algorithm
         T _end;
     };
 
+    namespace impl
+    {
+        template<typename IterType, typename Lock>
+        class GuardedRangeBase
+        {
+        protected:
+            GuardedRangeBase(IterType first, IterType last, Lock lock)
+                : first(std::move(first)), last(std::move(last)), lock(std::move(lock))
+            {}
+
+        public:
+            auto begin() const -> IterType {
+                return first;
+            }
+
+            auto end() const -> IterType {
+                return last;
+            }
+
+        private:
+            IterType first;
+            IterType last;
+            Lock lock;
+        };
+    } // namespace impl
+
+    /**
+     * @brief An iterator range guarded by an exclusive lock
+     */
     template<typename IterType, typename Mutex>
-    class GuardedRange
+    class GuardedRange : public impl::GuardedRangeBase<IterType, std::unique_lock<Mutex>>
     {
     public:
+        using Base = impl::GuardedRangeBase<IterType, std::unique_lock<Mutex>>;
+
         GuardedRange(IterType first, IterType last, std::unique_lock<Mutex> lock)
-            : first(std::move(first)), last(std::move(last)), lock(std::move(lock))
+            : Base(std::move(first), std::move(last), std::move(lock))
         {}
+    };
 
-        auto begin() const -> IterType {
-            return first;
-        }
+    /**
+     * @brief An iterator range guarded by a shared lock
+     */
+    template<typename IterType, typename Mutex>
+    class GuardedSharedRange : public impl::GuardedRangeBase<IterType, std::shared_lock<Mutex>>
+    {
+    public:
+        using Base = impl::GuardedRangeBase<IterType, std::shared_lock<Mutex>>;
 
-        auto end() const -> IterType {
-            return last;
-        }
-
-    private:
-        IterType first;
-        IterType last;
-        std::unique_lock<Mutex> lock;
+        GuardedSharedRange(IterType first, IterType last, std::shared_lock<Mutex> lock)
+            : Base(std::move(first), std::move(last), std::move(lock))
+        {}
     };
 } // namespace trc::algorithm
