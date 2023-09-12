a1c737d4fb07a2819dc1201d94d0cc03289fbdf4
Nicola Crivellin <nicola.crivellin98@gmail.com>
2023-09-12 15:20:38 +0200

    [medit] Implement highlight borders in renderer
    Always create a NodeInfo for new nodes in the material graph.

--- tools/material_editor/src/GraphRenderer.h
+++ tools/material_editor/src/GraphRenderer.h
@@ -8,6 +8,13 @@
 #include "GraphLayout.h"
 #include "MaterialGraph.h"
 
+namespace graph
+{
+    constexpr vec4 kNodeColor{ 0.025f, 0.025f, 0.025f, 1.0f };
+    constexpr vec4 kHighlightColor{ 1.0f, 0.7f, 0.2f, 1.0f };
+    constexpr vec4 kSelectColor{ 1.0f, 1.0f, 1.0f, 1.0f };
+} // namespace graph
+
 /**
  * @brief A renderable representation of a material graph
  */
@@ -15,17 +22,24 @@ struct GraphRenderData
 {
     void pushNode(vec2 pos, vec2 size, vec4 color);
     void pushSocket(vec2 pos, vec2 size, vec4 color);
-    void pushLink(vec2 from, vec2 to);
+    void pushLink(vec2 from, vec2 to, vec4 color);
+    void pushBorder(vec2 pos, vec2 size, vec4 color);
 
     void clear();
 
-    /** List of vec4{ posX, posY, sizeX, sizeY } */
-    std::vector<vec4> nodeDimensions;
-    std::vector<vec4> nodeColors;
+    enum class Geometry { eQuad, eLine };
 
-    /** List of vec4{ posX, posY, sizeX, sizeY } */
-    std::vector<vec4> socketDimensions;
-    std::vector<vec4> socketColors;
+    struct Primitive
+    {
+        Geometry geometryType;
+
+        /** List of vec4{ posX, posY, sizeX, sizeY } */
+        std::vector<vec4> dimensions{};
+        std::vector<vec4> colors{};
+    };
+
+    Primitive quads{ .geometryType=Geometry::eQuad };
+    Primitive lines{ .geometryType=Geometry::eLine };
 };
 
 /**
@@ -52,8 +66,14 @@ private:
 
     struct DeviceData
     {
-        uint32_t numNodes{ 0 };
-        uint32_t numSockets{ 0 };
+        struct DrawCmd
+        {
+            ui32 indexCount;
+            ui32 instanceCount;
+            trc::Pipeline::ID pipeline;
+        };
+
+        std::vector<DrawCmd> drawCmds;
 
         trc::Buffer dimensionBuf;
         trc::Buffer colorBuf;
