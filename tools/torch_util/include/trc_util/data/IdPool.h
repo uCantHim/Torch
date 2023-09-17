a3ceec2f4e0d069a7e7d57133e92233074527936
Nicola Crivellin <nicola.crivellin98@gmail.com>
2023-09-17 19:06:56 +0200

    Make data::IdPool moveable

--- tools/torch_util/include/trc_util/data/IdPool.h
+++ tools/torch_util/include/trc_util/data/IdPool.h
@@ -14,6 +14,29 @@ template<typename T = uint64_t>
 class IdPool
 {
 public:
+    IdPool(const IdPool&) = delete;
+    IdPool& operator=(const IdPool&) = delete;
+
+    IdPool() = default;
+    ~IdPool() noexcept = default;
+
+    IdPool(IdPool&& other) noexcept
+        : nextId(T{other.nextId}), freeIds(std::move(other.freeIds))
+    {
+        other.nextId = T{0};
+    }
+
+    IdPool& operator=(IdPool&& rhs) noexcept
+    {
+        if (this != &rhs)
+        {
+            nextId = T{rhs.nextId};
+            rhs.nextId = 0;
+            freeIds = std::move(rhs.freeIds);
+        }
+        return *this;
+    }
+
     auto generate() -> T
     {
         if (auto id = freeIds.try_pop()) {
