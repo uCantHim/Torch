83add2d5216230c79cbf688674543ca43b601279
Nicola Crivellin <nicola.crivellin98@gmail.com>
2023-09-17 16:26:08 +0200

    [medit] Implement text

--- tools/material_editor/shaders/graph_element.vert
+++ tools/material_editor/shaders/graph_element.vert
@@ -3,7 +3,8 @@
 layout (location = 0) in vec2 vertexPosition;
 layout (location = 1) in vec2 vertexUv;
 layout (location = 2) in vec4 itemDims;
-layout (location = 3) in vec4 itemColor;
+layout (location = 3) in vec4 itemUvDims;
+layout (location = 4) in vec4 itemColor;
 
 layout (set = 0, binding = 0) uniform CameraData
 {
@@ -21,11 +22,15 @@ layout (location = 0) out PerVertex
 
 void main()
 {
-    const vec2 itemPos = vec2(itemDims.xy);
-    const vec2 itemSize = vec2(itemDims.zw);
+    const vec2 itemPos = itemDims.xy;
+    const vec2 itemSize = itemDims.zw;
     const vec2 worldPos = itemPos + itemSize * vertexPosition;
 
+    const vec2 itemUvOffset = itemUvDims.xy;
+    const vec2 itemUvSize = itemUvDims.zw;
+    const vec2 uv = itemUvOffset + itemUvSize * vertexUv;
+
     gl_Position = projMatrix * viewMatrix * vec4(worldPos, 0.0f, 1.0f);
-    vert.uv = vertexUv;
+    vert.uv = uv;
     vert.color = itemColor;
 }
--- tools/material_editor/src/GraphRenderer.cpp
+++ tools/material_editor/src/GraphRenderer.cpp
@@ -4,67 +4,110 @@
 #include <numeric>
 #include <ranges>
 
+#include <trc/DescriptorSetUtils.h>
+#include <trc/base/Barriers.h>
+#include <trc/base/ImageUtils.h>
 #include <trc_util/algorithm/VectorTransform.h>
 
 #include "pipelines.h"
 
+// Get an iterable list of all types of primitives in a draw group
+#define allPrimitives(group) { (group).quads, (group).glyphs, (group).lines }
+
+
+
+void GraphRenderData::Primitive::pushItem(vec4 dim, vec4 uvDim, vec4 color)
+{
+    dimensions.emplace_back(dim);
+    uvDims.emplace_back(uvDim);
+    colors.emplace_back(color);
+}
+
+void GraphRenderData::Primitive::pushDefaultUvs()
+{
+    uvDims.emplace_back(kDefaultUv);
+}
+
+
 
 void GraphRenderData::beginGroup()
 {
-    items.emplace_back();
+    groups.emplace_back();
 }
 
 auto GraphRenderData::curGroup() -> DrawGroup&
 {
-    return items.back();
+    return groups.back();
 }
 
 void GraphRenderData::pushNode(vec2 pos, vec2 size, vec4 color)
 {
-    curGroup().quads.dimensions.emplace_back(pos, size);
-    curGroup().quads.colors.emplace_back(color);
+    curGroup().quads.pushItem({ pos, size }, kDefaultUv, color);
 }
 
 void GraphRenderData::pushSocket(vec2 pos, vec2 size, vec4 color)
 {
-    curGroup().quads.dimensions.emplace_back(pos, size);
-    curGroup().quads.colors.emplace_back(color);
+    curGroup().quads.pushItem({ pos, size }, kDefaultUv, color);
 }
 
 void GraphRenderData::pushLink(vec2 from, vec2 to, vec4 color)
 {
-    curGroup().lines.dimensions.emplace_back(from, to - from);
-    curGroup().lines.colors.emplace_back(color);
+    curGroup().lines.pushItem({ from, to - from }, kDefaultUv, color);
+}
+
+void GraphRenderData::pushText(vec2 pos, float scale, std::string_view str, vec4 color, Font& font)
+{
+    for (char c : str)
+    {
+        const auto& meta = font.getMetadata(c).metaNormalized;
+        const auto& g = font.getGlyph(c);
+
+        // Correct position for Vulkan's flipped y-axis
+        const vec2 gPos{ pos.x, pos.y + (font.maxAscendNorm - meta.bearingY) * scale };
+        const vec2 gSize{ g.size * scale };
+        curGroup().glyphs.pushItem({ gPos, gSize }, { g.uvPos, g.uvSize }, color);
+
+        pos.x += meta.advance * scale;
+    }
+}
+
+void GraphRenderData::pushLetter(vec2 pos, float scale, trc::CharCode c, vec4 color, Font& font)
+{
+    const auto& g = font.getGlyph(c);
+    curGroup().glyphs.dimensions.emplace_back(pos, g.size * scale);
+    curGroup().glyphs.uvDims.emplace_back(g.uvPos, g.uvSize);
+    curGroup().glyphs.colors.emplace_back(color);
 }
 
 void GraphRenderData::pushBorder(vec2 pos, vec2 size, vec4 color)
 {
-    curGroup().lines.dimensions.emplace_back(pos, vec2{ size.x, 0.0f });
-    curGroup().lines.dimensions.emplace_back(pos, vec2{ 0.0f, size.y });
-    curGroup().lines.dimensions.emplace_back(pos + size, vec2{ -size.x, 0.0f });
-    curGroup().lines.dimensions.emplace_back(pos + size, vec2{ 0.0f, -size.y });
-    curGroup().lines.colors.emplace_back(color);
-    curGroup().lines.colors.emplace_back(color);
-    curGroup().lines.colors.emplace_back(color);
-    curGroup().lines.colors.emplace_back(color);
+    curGroup().lines.pushItem({ pos, vec2{ size.x, 0.0f } },         kDefaultUv, color);
+    curGroup().lines.pushItem({ pos, vec2{ 0.0f, size.y } },         kDefaultUv, color);
+    curGroup().lines.pushItem({ pos + size, vec2{ -size.x, 0.0f } }, kDefaultUv, color);
+    curGroup().lines.pushItem({ pos + size, vec2{ 0.0f, -size.y } }, kDefaultUv, color);
 }
 
 void GraphRenderData::pushSeparator(vec2 pos, float size, vec4 color)
 {
-    curGroup().lines.dimensions.emplace_back(pos, vec2(size, 0.0f));
-    curGroup().lines.colors.emplace_back(color);
+    curGroup().lines.pushItem({ pos, vec2(size, 0.0f) }, kDefaultUv, color);
 }
 
 void GraphRenderData::clear()
 {
     curGroup().quads.dimensions.clear();
+    curGroup().quads.uvDims.clear();
     curGroup().quads.colors.clear();
     curGroup().lines.dimensions.clear();
+    curGroup().lines.uvDims.clear();
     curGroup().lines.colors.clear();
+    curGroup().glyphs.dimensions.clear();
+    curGroup().glyphs.uvDims.clear();
+    curGroup().glyphs.colors.clear();
 }
 
 void renderNodes(
     const GraphScene& scene,
+    Font& font,
     GraphRenderData& res)
 {
     auto calcSocketColor = [&scene](SocketID sock) -> vec4 {
@@ -77,12 +120,14 @@ void renderNodes(
     const auto& graph = scene.graph;
     const auto& layout = scene.layout;
 
-    for (const auto& [node, _] : graph.nodeInfo.items())
+    for (const auto& [node, nodeInfo] : graph.nodeInfo.items())
     {
         res.beginGroup();
 
         const auto& [nodePos, nodeSize] = layout.nodeSize.get(node);
+        const vec2 titlePos = calcTitleTextPos(node, layout);
         res.pushNode(nodePos, nodeSize, graph::kNodeColor);
+        res.pushText(titlePos, graph::kTextHeight, nodeInfo.desc.name, graph::kTextColor, font);
         res.pushSeparator(nodePos + vec2(0.0f, graph::kNodeHeaderHeight),
                           nodeSize.x,
                           graph::kSeparatorColor);
@@ -136,13 +181,13 @@ void renderSelectionBox(const GraphInteraction& interaction, GraphRenderData& re
     }
 }
 
-auto buildRenderData(const GraphScene& scene) -> GraphRenderData
+auto buildRenderData(const GraphScene& scene, Font& font) -> GraphRenderData
 {
     const auto& graph = scene.graph;
     const auto& layout = scene.layout;
 
     GraphRenderData res;
-    renderNodes(scene, res);
+    renderNodes(scene, font, res);
     renderSocketLinks(graph, layout, res);
     renderSelectionBox(scene.interaction, res);
 
@@ -164,9 +209,14 @@ auto makeVertBuf(const trc::Device& device, size_t size) -> trc::Buffer
     };
 }
 
-MaterialGraphRenderer::MaterialGraphRenderer(const trc::Device& device, const trc::FrameClock& clock)
+MaterialGraphRenderer::MaterialGraphRenderer(
+    const trc::Device& device,
+    const trc::FrameClock& clock,
+    const trc::Image& fontImage)
     :
     device(device),
+    descProvider({}, {}),
+    baseTexture(trc::makeSinglePixelImage(device, vec4(1.0f))),
     vertexBuf(
         device,
         []{
@@ -187,29 +237,104 @@ MaterialGraphRenderer::MaterialGraphRenderer(const trc::Device& device, const tr
         DeviceData res{
             .drawCmds={},
             .dimensionBuf=makeVertBuf(device, kElemDimSize * 2000),
+            .uvBuf=makeVertBuf(device, kElemUvSize * 2000),
             .colorBuf=makeVertBuf(device, kElemColorSize * 2000),
             .dimensions=nullptr,
+            .uvs=nullptr,
             .colors=nullptr,
         };
         res.dimensions = res.dimensionBuf.map<vec4*>();
+        res.uvs = res.uvBuf.map<vec4*>();
         res.colors = res.colorBuf.map<vec4*>();
 
         return res;
     })
 {
-    auto pl = getGraphElemPipeline();
-    trc::PipelineRegistry::cloneGraphicsPipeline(pl);
+    constexpr auto imgLayout = vk::ImageLayout::eShaderReadOnlyOptimal;
+    constexpr auto descType = vk::DescriptorType::eCombinedImageSampler;
+    constexpr size_t setCount = 2;
+
+    // Create texture descriptions
+    textures[baseTextureIndex] = Texture{
+        .imageView=baseTexture.createView(vk::ImageAspectFlagBits::eColor),
+        .sampler=device->createSamplerUnique(
+            vk::SamplerCreateInfo{
+                vk::SamplerCreateFlags{},
+                vk::Filter::eNearest, vk::Filter::eNearest,
+                vk::SamplerMipmapMode::eNearest,
+                vk::SamplerAddressMode::eRepeat,
+                vk::SamplerAddressMode::eRepeat,
+                vk::SamplerAddressMode::eRepeat,
+                0.0f, false, 0.0f, false, vk::CompareOp::eNever,
+                0.0f, 0.0f, vk::BorderColor::eFloatOpaqueWhite, false
+            }
+        ),
+        .descSet{},
+    };
+    textures[fontTextureIndex] = Texture{
+        .imageView=fontImage.createView(vk::ImageAspectFlagBits::eColor),
+        .sampler=device->createSamplerUnique(
+            vk::SamplerCreateInfo{ {}, vk::Filter::eLinear, vk::Filter::eLinear, }
+        ),
+        .descSet{},
+    };
+
+    // Bring base texture into the correct format
+    device.executeCommands(trc::QueueType::graphics, [&](vk::CommandBuffer cmdBuf) {
+        trc::imageMemoryBarrier(cmdBuf,
+            *baseTexture, vk::ImageLayout::eUndefined, imgLayout, {}, {}, {}, {},
+            vk::ImageSubresourceRange(vk::ImageAspectFlagBits::eColor, 0, 1, 0, 1)
+        );
+    });
+
+    // Create descriptors
+    auto b = trc::buildDescriptorSetLayout()
+        .addBinding(descType, 1, vk::ShaderStageFlagBits::eFragment);
+    pool = b.buildPool(device, setCount, vk::DescriptorPoolCreateFlagBits::eFreeDescriptorSet);
+    layout = b.build(device);
+    descProvider = { *layout, {} };
+
+    std::vector<vk::DescriptorSetLayout> numSets(setCount, *layout);
+    auto sets = device->allocateDescriptorSetsUnique({ *pool, numSets });
+    textures[baseTextureIndex].descSet = std::move(sets[0]);
+    textures[fontTextureIndex].descSet = std::move(sets[1]);
+
+    // Update descriptor sets
+    auto imgInfo = textures | std::views::transform(
+        [](Texture& tex) {
+            return vk::DescriptorImageInfo{ *tex.sampler, *tex.imageView, imgLayout };
+        }
+    );
+
+    std::vector<vk::DescriptorImageInfo> imgInfos(imgInfo.begin(), imgInfo.end());
+    device->updateDescriptorSets({
+        vk::WriteDescriptorSet{ *textures[0].descSet, 0, 0, descType, imgInfos[0] },
+        vk::WriteDescriptorSet{ *textures[1].descSet, 0, 0, descType, imgInfos[1] },
+    }, {});
 }
 
 void MaterialGraphRenderer::uploadData(const GraphRenderData& data)
 {
     constexpr auto getIndexCount = [](GraphRenderData::Geometry g) {
-        return g == GraphRenderData::Geometry::eQuad ? 6u : 2u;
+        switch (g) {
+            case GraphRenderData::Geometry::eQuad: return 6u;
+            case GraphRenderData::Geometry::eLine: return 2u;
+            case GraphRenderData::Geometry::eGlyph: return 6u;
+        } throw std::logic_error("");
+    };
+    constexpr auto getTexture = [](GraphRenderData::Geometry g) {
+        switch (g) {
+            case GraphRenderData::Geometry::eQuad: return baseTextureIndex;
+            case GraphRenderData::Geometry::eLine: return baseTextureIndex;
+            case GraphRenderData::Geometry::eGlyph: return fontTextureIndex;
+        } throw std::logic_error("");
     };
     constexpr auto getPipeline = [](GraphRenderData::Geometry g) {
-        return g == GraphRenderData::Geometry::eQuad
-               ? getGraphElemPipeline()
-               : getLinePipeline();
+        switch (g) {
+            case GraphRenderData::Geometry::eQuad: return getGraphElemPipeline();
+            case GraphRenderData::Geometry::eLine: return getLinePipeline();
+            case GraphRenderData::Geometry::eGlyph: return getGraphElemPipeline();
+        } throw std::logic_error("");
     };
 
     auto& dev = *deviceData;
@@ -218,26 +343,28 @@ void MaterialGraphRenderer::uploadData(const GraphRenderData& data)
     ensureBufferSize(data, dev, device);
 
     size_t totalOffset{ 0 };  // Offset into the vertex buffers in number of primitives
-    for (const auto& data : data.items)
+    for (const auto& group : data.groups)
     {
-        assert(data.quads.colors.size() == data.quads.dimensions.size());
-        assert(data.lines.colors.size() == data.lines.dimensions.size());
-
-        // Modify this if you add a new type of primitive
-        auto primitives = { data.quads, data.lines };
+        assert(group.quads.colors.size() == group.quads.dimensions.size());
+        assert(group.lines.colors.size() == group.lines.dimensions.size());
+        assert(group.glyphs.colors.size() == group.glyphs.dimensions.size());
 
         // Copy primitive data to device
-        for (const auto& prim : primitives)
+        for (const auto& prim : allPrimitives(group))
         {
             const auto& draw = dev.drawCmds.emplace_back(DeviceData::DrawCmd{
                 .indexCount    = getIndexCount(prim.geometryType),
                 .instanceCount = static_cast<ui32>(prim.dimensions.size()),
+                .textureIndex  = getTexture(prim.geometryType),
                 .pipeline      = getPipeline(prim.geometryType),
             });
 
             memcpy(dev.dimensions + totalOffset,
                    prim.dimensions.data(),
                    prim.dimensions.size() * kElemDimSize);
+            memcpy(dev.uvs + totalOffset,
+                   prim.uvDims.data(),
+                   prim.uvDims.size() * kElemUvSize);
             memcpy(dev.colors + totalOffset,
                    prim.colors.data(),
                    prim.colors.size() * kElemColorSize);
@@ -255,25 +382,30 @@ void MaterialGraphRenderer::ensureBufferSize(
     const trc::Device& device)
 {
     size_t numElems{ 0 };
-    for (const auto& cmd : renderData.items)
+    for (const auto& group : renderData.groups)
     {
-        auto primitives = { cmd.lines, cmd.quads };
-        auto counts = primitives | std::views::transform([](auto&& el){ return el.dimensions.size(); });
-        numElems += std::accumulate(counts.begin(), counts.end(), 0);
+        for (auto& prim : allPrimitives(group)) {
+            numElems += prim.dimensions.size();
+        }
     }
 
     const size_t dimBufSize = numElems * kElemDimSize;
+    const size_t uvBufSize = numElems * kElemUvSize;
     const size_t colorBufSize = numElems * kElemColorSize;
     if (dev.dimensionBuf.size() < dimBufSize)
     {
         assert(dev.colorBuf.size() < colorBufSize);
+        assert(dev.uvBuf.size() < colorBufSize);
 
         dev.dimensionBuf.unmap();
+        dev.uvBuf.unmap();
         dev.colorBuf.unmap();
 
         dev.dimensionBuf = makeVertBuf(device, dimBufSize);
+        dev.uvBuf = makeVertBuf(device, uvBufSize);
         dev.colorBuf = makeVertBuf(device, colorBufSize);
         dev.dimensions = dev.dimensionBuf.map<vec4*>();
+        dev.uvs = dev.uvBuf.map<vec4*>();
         dev.colors = dev.colorBuf.map<vec4*>();
     }
 }
@@ -293,13 +425,23 @@ void MaterialGraphRenderer::draw(vk::CommandBuffer cmdBuf, trc::RenderConfig& co
     {
         if (draw.instanceCount == 0) continue;
 
-        conf.getPipeline(draw.pipeline).bind(cmdBuf, conf);
+        auto& pipeline = conf.getPipeline(draw.pipeline);
+        pipeline.bind(cmdBuf, conf);
+        cmdBuf.bindDescriptorSets(
+            vk::PipelineBindPoint::eGraphics, *pipeline.getLayout(),
+            1, *textures[draw.textureIndex].descSet, {}
+        );
 
         cmdBuf.bindVertexBuffers(2,
-            { *data.dimensionBuf,    *data.colorBuf },
-            { kElemDimSize * offset, kElemColorSize * offset }
+            { *data.dimensionBuf,    *data.uvBuf,          *data.colorBuf },
+            { kElemDimSize * offset, kElemUvSize * offset, kElemColorSize * offset }
         );
         cmdBuf.drawIndexed(draw.indexCount, draw.instanceCount, 0, 0, 0);
         offset += draw.instanceCount;
     }
+}
+
+auto MaterialGraphRenderer::getTextureDescriptor() const -> const trc::DescriptorProviderInterface&
+{
+    return descProvider;
 }
