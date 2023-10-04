4960c6090ccba4479e7f527551742534a66c9b85
Nicola Crivellin <nicola.crivellin98@gmail.com>
2023-10-04 18:04:35 +0200

    [medit] Implement text input for input field
    Receive and display user input in text/number input fields. Also display
    a fancy 'cursor line' when an input field is selected.

--- tools/material_editor/src/GraphInteraction.h
+++ tools/material_editor/src/GraphInteraction.h
@@ -14,15 +14,4 @@ struct GraphInteraction
     std::unordered_set<NodeID> selectedNodes;
 
     std::optional<Hitbox> multiSelectBox;
-
-    // Text input fields are used to input floating-point numbers
-    Table<std::string, SocketID> textInputFields;
-    Table<vec4, SocketID> colorInputFields;
 };
-
-void createInputFields(GraphInteraction& inter, NodeID node, const GraphTopology& graph);
-
-/**
- * @brief Update output values of sockets from their input field decorations.
- */
-void updateOutputValues(const GraphInteraction& in, GraphTopology& graph);
