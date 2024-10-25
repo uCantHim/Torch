85fcdf13605b4279c7b340e88d0ca0e692d3919f
Nicola Crivellin <nicola.crivellin98@gmail.com>
2024-10-25 17:01:55 +0200

    Use std::generator to clean up RasterSceneBase
    Also remove obsolete parameters from the DrawEnvironment struct. Move
    upload of the shadow matrix index to the shadow draw task; allows us to
    remove the hacky dynamic_cast in the draw function.

--- src/RasterTasks.cpp
+++ src/RasterTasks.cpp
@@ -42,11 +42,11 @@ void RenderPassDrawTask::record(vk::CommandBuffer cmdBuf, ViewportDrawContext& c
             p.bind(cmdBuf, ctx.resources());
 
             // Record commands for all objects with this pipeline
-            scene.invokeDrawFunctions(
-                renderStage, *renderPass, SubPass::ID(subPass),
-                pipeline, p,
-                cmdBuf
-            );
+            const DrawEnvironment env{ .currentPipeline = &p };
+
+            for (auto& func : scene.iterDrawFunctions(renderStage, subpass, pipeline)) {
+                func(env, cmdBuf);
+            }
         }
 
         if (subPass < subPassCount - 1) {
@@ -82,12 +82,16 @@ void ShadowMapDrawTask::record(vk::CommandBuffer cmdBuf, SceneUpdateContext& ctx
             auto& p = ctx.resources().getPipeline(pipeline);
             p.bind(cmdBuf, ctx.resources());
 
+            // Set renderpass-specific data
+            cmdBuf.pushConstants<ui32>(*p.getLayout(), vk::ShaderStageFlagBits::eVertex,
+                                       sizeof(mat4), renderPass.getShadowMatrixIndex());
+
             // Record commands for all objects with this pipeline
-            scene.invokeDrawFunctions(
-                renderStage, renderPass, SubPass::ID(subPass),
-                pipeline, p,
-                cmdBuf
-            );
+            const DrawEnvironment env{ .currentPipeline = &p };
+
+            for (auto& func : scene.iterDrawFunctions(renderStage, subpass, pipeline)) {
+                func(env, cmdBuf);
+            }
         }
 
         if (subPass < subPassCount - 1) {
