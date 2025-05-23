5cde2781a5a59c3ca0f61d68b80edcd79b7a2e51
Nicola Crivellin <nicola.crivellin98@gmail.com>
2025-05-23 16:28:29 +0200

    Add a std::format-like interface to Logger
    Also add a runtime log level setting. The default considers the
    environment variable TORCH_LOG_LEVEL.

--- include/trc/base/Logging.h
+++ include/trc/base/Logging.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <format>
 #include <functional>
 #include <ostream>
 #include <source_location>
@@ -11,27 +12,62 @@ namespace trc
     {
         enum class LogLevel
         {
+            // Detailed information that is useful to developers.
             eDebug,
+
+            // Messages that confirm or illustrate correct program behaviour.
             eInfo,
+
+            // Potentially dangerous behaviour. The program may not work as
+            // intended in the future because of the reasons illustrated here.
             eWarning,
+
+            // Incorrect behaviour that will cause unintended effects, but not
+            // be fatal to the overall program execution.
             eError,
+
+            // Unrecoverable errors that will likely cause the program to exit
+            // or crash.
+            eCritical,
         };
 
         /**
          * @return bool True if the specified log level is enabled by the
          *              applicable compile-time rules.
          */
-        consteval bool isStaticallyEnabled(LogLevel level) {
+        constexpr bool isStaticallyEnabled(LogLevel level) {
             return level >= TRC_LOG_LEVEL;
         }
+
+        /**
+         * @brief Set the global log level.
+         *
+         * Only messages at or above the global log level are logged.
+         */
+        void setLogLevel(LogLevel level);
+
+        /**
+         * @brief Determine whether a log level is currently enabled.
+         *
+         * @return bool True if `level` is currently enabled by both compile-
+         *              time and runtime rules, the latter being the global log
+         *              level set by `setLogLevel`.
+         */
+        bool isEnabled(LogLevel level);
     } // namespace log
 
-    template<log::LogLevel level>
+    /**
+     * @brief A logger.
+     *
+     * @tparam bool kStaticEnable Enables or disables logging at compile time.
+     *                            Can allow lots of compiler optimization for
+     *                            statically disabled loggers, reducing runtime
+     *                            overhead.
+     */
+    template<log::LogLevel level, bool kStaticEnable = log::isStaticallyEnabled(level)>
     class Logger
     {
     public:
-        static constexpr bool kEnable = log::isStaticallyEnabled(level);
-
         struct LogEntry
         {
             LogEntry(const LogEntry&) = delete;
@@ -42,28 +78,44 @@ namespace trc
             LogEntry(LogEntry&& other) noexcept
                 : os(other.os), isOwning(true)
             {
-                other.isOwning = false;
+                if (this != &other) {
+                    other.isOwning = false;
+                }
             }
 
             ~LogEntry()
             {
-                if constexpr (kEnable) {
+                if (log::isEnabled(level)) {
                     if (isOwning) os << "\n";
                 }
             }
 
             template<typename T>
             auto operator<<(T&& v) -> LogEntry&
+                requires requires (std::ostream& os) { os << std::forward<T>(v); }
             {
-                if constexpr (kEnable) {
+                if (log::isEnabled(level)) {
                     os << std::forward<T>(v);
+                }
+                return *this;
+            }
+
+            /**
+             * This overload is needed because, apparently, we cannot infer the
+             * template argument type when passing a stream manipulator to
+             * `operator<<(T&&)`.
+             */
+            auto operator<<(std::ostream& (*streamManip)(std::ostream&)) -> LogEntry&
+            {
+                if (log::isEnabled(level)) {
+                    os << streamManip;
                 }
                 return *this;
             }
 
             auto operator<<(std::source_location loc) -> LogEntry&
             {
-                if constexpr (kEnable)
+                if (log::isEnabled(level))
                 {
                     os << "["
                        << loc.file_name() << ":" << loc.line()
@@ -78,6 +130,21 @@ namespace trc
             bool isOwning{ true };
         };
 
+        struct NullLogEntry
+        {
+            NullLogEntry() = default;
+            explicit NullLogEntry(std::ostream&) {}
+
+            auto operator<<(auto&&) -> NullLogEntry& {
+                return *this;
+            }
+            auto operator<<(std::ostream& (*)(std::ostream&)) -> NullLogEntry& {
+                return *this;
+            }
+        };
+
+        using LogEntryT = std::conditional_t<kStaticEnable, LogEntry, NullLogEntry>;
+
         Logger(const Logger&) = delete;
         Logger& operator=(const Logger&) = delete;
 
@@ -90,24 +157,61 @@ namespace trc
             : stream(&os), makeHeader(std::move(header))
         {}
 
-        auto startEntry() -> LogEntry
+        auto startEntry() -> LogEntryT
         {
-            LogEntry entry{ *stream };
+            LogEntryT entry{ *stream };
             entry << makeHeader() << " ";
             return entry;
         }
 
         template<typename T>
-        auto operator<<(T&& v) -> LogEntry
+        auto operator<<(T&& v) -> LogEntryT
         {
-            auto entry = startEntry();
-            entry << std::forward<T>(v);
-            return entry;
+            return std::move(startEntry() << std::forward<T>(v));
         }
 
+        /**
+         * @brief A std::format interface.
+         */
+        template<typename... Args>
+        void operator()([[maybe_unused]] std::format_string<Args...> fmt,
+                        [[maybe_unused]] Args&&... args)
+        {
+            if constexpr (kStaticEnable) {
+                *stream << std::format(fmt, std::forward<Args>(args)...) << "\n";
+            }
+        }
+
+        /**
+         * @brief A std::format interface.
+         */
+        template<typename... Args>
+        void operator()([[maybe_unused]] const std::locale& loc,
+                        [[maybe_unused]] std::format_string<Args...> fmt,
+                        [[maybe_unused]] Args&&... args)
+        {
+            if constexpr (kStaticEnable) {
+                *stream << std::format(loc, fmt, std::forward<Args>(args)...) << "\n";
+            }
+        }
+
+        /**
+         * @brief Set the underlying stream to which the logger writes messages.
+         */
         void setOutputStream(std::ostream& os)
         {
             stream = &os;
+        }
+
+        void setHeader(std::function<std::string()> headerFunc) {
+            makeHeader = std::move(headerFunc);
+        }
+
+        /**
+         * @brief Flush the underlying stream.
+         */
+        void flush() {
+            stream->flush();
         }
 
     private:
@@ -117,19 +221,18 @@ namespace trc
 
     namespace log
     {
-        extern Logger<LogLevel::eDebug>   debug;
-        extern Logger<LogLevel::eInfo>    info;
-        extern Logger<LogLevel::eWarning> warn;
-        extern Logger<LogLevel::eError>   error;
+        extern Logger<LogLevel::eDebug>    debug;
+        extern Logger<LogLevel::eInfo>     info;
+        extern Logger<LogLevel::eWarning>  warn;
+        extern Logger<LogLevel::eError>    error;
+        extern Logger<LogLevel::eCritical> critical;
 
         /**
          * @brief Insert the current source location into a stream.
          *
          * Example:
-         * ```cpp
          *
-         * log::error << here() << ": An error has occurred!";
-         * ```
+         *     log::error << here() << ": An error has occurred!";
          */
         constexpr auto here(std::source_location loc = std::source_location::current()) {
             return loc;
@@ -140,16 +243,14 @@ namespace trc
          *
          * The header prints the current time and a message severity.
          *
-         * @param std::string_view messageSeverity
+         * @param std::string messageSeverity
          *
          * # Example
-         * ```cpp
          *
-         * std::ofstream myLogFile{ "/var/log/my_log.txt" };
-         * Logger<true> myLog{ myLogFile, log::makeDefaultLogHeader("ERROR") };
-         * ```
+         *     std::ofstream myLogFile{ "/var/log/my_log.txt" };
+         *     Logger<LogLevel::eError> myLog{ myLogFile, log::makeDefaultLogHeader("ERROR") };
          */
-        auto makeDefaultLogHeader(std::string_view messageSeverity)
+        auto makeDefaultLogHeader(std::string messageSeverity)
             -> std::function<std::string()>;
     }
 } // namespace trc
--- src/base/Logging.cpp
+++ src/base/Logging.cpp
@@ -1,5 +1,7 @@
 #include "trc/base/Logging.h"
 
+#include <cstdlib>
+
 #include <chrono>
 #include <iostream>
 #include <format>
@@ -8,10 +10,45 @@
 
 namespace trc::log
 {
+    LogLevel globalLogLevel = []{
+        using namespace std::string_view_literals;
+
+        // Override the compile-time choice of the default log level if one is
+        // specified by the environment.
+        auto envStr = std::getenv(TRC_LOG_LEVEL_ENV_NAME);
+        if (!envStr) {
+            return TRC_LOG_LEVEL;
+        }
+        if (envStr == "Critical"sv) {
+            return LogLevel::eCritical;
+        }
+        if (envStr == "Error"sv) {
+            return LogLevel::eError;
+        }
+        if (envStr == "Warning"sv) {
+            return LogLevel::eWarning;
+        }
+        if (envStr == "Info"sv) {
+            return LogLevel::eInfo;
+        }
+        if (envStr == "Debug"sv) {
+            return LogLevel::eDebug;
+        }
+        return TRC_LOG_LEVEL;
+    }();
+
+    void setLogLevel(LogLevel level)
+    {
+        globalLogLevel = level;
+    }
+
+    bool isEnabled(LogLevel level)
+    {
+        return isStaticallyEnabled(level) && level >= globalLogLevel;
+    }
+
     struct DefaultLogHeader
     {
-        explicit DefaultLogHeader(std::string_view severity) : severity(severity) {}
-
         auto operator()() const -> std::string
         {
             auto time = std::chrono::current_zone()->to_local(std::chrono::system_clock::now());
@@ -23,16 +60,17 @@ namespace trc::log
             return std::format("[{:%F %X}] --{}-- ", time, severity);
         }
 
-        std::string_view severity;
+        std::string severity;
     };
 
-    auto makeDefaultLogHeader(std::string_view messageSeverity) -> std::function<std::string()>
+    auto makeDefaultLogHeader(std::string messageSeverity) -> std::function<std::string()>
     {
-        return DefaultLogHeader{ messageSeverity };
+        return DefaultLogHeader{ std::move(messageSeverity) };
     }
 
-    Logger<LogLevel::eDebug>   debug(std::cout, DefaultLogHeader{ "DEBUG" });
-    Logger<LogLevel::eInfo>    info(std::cout, DefaultLogHeader{ "INFO" });
-    Logger<LogLevel::eWarning> warn(std::cout, DefaultLogHeader{ "WARNING" });
-    Logger<LogLevel::eError>   error(std::cout, DefaultLogHeader{ "ERROR" });
+    Logger<LogLevel::eDebug>    debug(std::cout, DefaultLogHeader{ "DEBUG" });
+    Logger<LogLevel::eInfo>     info(std::cout, DefaultLogHeader{ "INFO" });
+    Logger<LogLevel::eWarning>  warn(std::cout, DefaultLogHeader{ "WARNING" });
+    Logger<LogLevel::eError>    error(std::cout, DefaultLogHeader{ "ERROR" });
+    Logger<LogLevel::eCritical> critical(std::cout, DefaultLogHeader{ "CRITICAL" });
 }
